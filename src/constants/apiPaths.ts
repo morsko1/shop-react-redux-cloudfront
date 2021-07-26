5363dcc9a96236676058a25230ad850b29a250a0
morsko1 <morssko1@gmail.com>
2021-07-26 15:43:39 +0300

    updated import api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://284w5ktc8h.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://l781ide91b.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://tju5sz36mk.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
